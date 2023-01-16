3b11b857c94d9a8c4cab14a5bb02ff6c3b9d82da
vlad blinov <vladivislav@yandex.ru>
2023-01-16 09:27:51 +0300

    estethical fix of name answering

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ import readlineSync from 'readline-sync';
 
 export const greetings = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name?');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
