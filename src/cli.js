bd863f153e960ef442b9910d30aaeeb593a9f279
vlad blinov <vladivislav@yandex.ru>
2023-01-16 09:17:26 +0300

    fix linting problems in cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable import/prefer-default-export */
 import readlineSync from 'readline-sync';
+
 export const greetings = () => {
-    console.log("Welcome to the Brain Games!");
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}!`);
-}
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
+};
